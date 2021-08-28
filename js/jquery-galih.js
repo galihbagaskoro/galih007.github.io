312801057a25e16e9cb8409e622ec9dbac09cd25
galihjp99 <83418945+galihjp99@users.noreply.github.com>
2021-08-28 15:00:35 +0700

    Initial commit

---
+++ js/jquery-galih.js
@@ -0,0 +1,13 @@
+var menu = document.getElementById("menu");
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener("click", function () {
+		var current = document.getElementsByClassName("active");
+		if (current.length > 0) {
+			current[0].className = current[0].className.replace("active", "");
+		}
+		this.className += " active";
+	});
+}
+document.documentElement.scrollTop = 0;
+document.body.scrollTop = 0;
